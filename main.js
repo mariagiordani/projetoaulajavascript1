66606944149895e373d167485d46e29744aaad94
mariagiordane <58854064+mariagiordane@users.noreply.github.com>
2020-01-22 17:16:10 -0300

    Initial-Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var nome = "Maria Giordani";
+var n1 = 29;
+var n2 = 10
+var frase = "Japão é o melhor time do mundo"
+alert(nome + " tem " + idade + " anos");
+
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase);
+console.log(frase.toLowerCase);
