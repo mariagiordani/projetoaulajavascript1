ff471d3a200beb02c7efa8247c15bc18f55f25f6
mariagiordane <58854064+mariagiordane@users.noreply.github.com>
2020-01-22 17:31:16 -0300

    update-js

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-var nome = "Maria Giordani";
-var n1 = 29;
-var n2 = 10
-var frase = "Japão é o melhor time do mundo"
-alert(nome + " tem " + idade + " anos");
+//var frutas = [{nome: "maca", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+//console.log(frutas.nome);
+//alert(frutas[1].cor);
 
-console.log(nome);
-console.log(n1 + n2);
-console.log(frase.replace("Japão","Brasil"));
-console.log(frase.toUpperCase);
-console.log(frase.toLowerCase);
+
+/*
+var fruta = {nome: "maca", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*
+
+//var lista = ["maca", "pera", "laranja"];
+//lista.push("uva";)
+//lista.pop("pera");
+//lista.length; //tamanho da lista
+//lista.reverse; //itens ao contratio na lista
+//console.log(lista[1]);
+//console.log(lista.toString());
+//console.log(lista.join(" - ");
+
+
+//var nome = "Maria Giordani";
+//var n1 = 29;
+//var n2 = 10
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase);
+//console.log(frase.toLowerCase);
