e927466d983ab0e2479d426c15d864e54caa2e33
mariagiordane <58854064+mariagiordane@users.noreply.github.com>
2020-01-23 12:16:42 -0300

    update-mainjs3

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getSeconds);
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count = count + 1; OU count++
+};
+*
+
+/*
 var idade = prompt("Qual sua idade");
 if (idade >= 18){
     alert("maior de idade");
 }else{
     alert("menor de idade");
 };
-
+*
 
 //var frutas = [{nome: "maca", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
 //console.log(frutas.nome);
